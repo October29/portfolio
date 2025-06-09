fde4d0145313d4ebac03e9f11ab047609a8ca9e2
october29 <fpuertas1992@proton.me>
2025-06-09 16:45:30 -0500

    refactor: load images from projectImages.data

--- src/components/brandingItem/BrandingItem.jsx
+++ src/components/brandingItem/BrandingItem.jsx
@@ -1,4 +1,5 @@
 import style from './BrandingItem.module.css';
+import bradingImages from '../../data/projectsImages.data'
 
 const BrandingItem = () => {
   return (
@@ -13,62 +14,20 @@ Adonde nadie nunca supiera del papa de Rafael Santos
 
 Porque una pena tras otra pena estan acabando conmigo
 Y yo por ser un hombre tan fuerte he podido resistir
-Ay y no me quiero morir porque me duelen mis hijos
-
-Porque una pena tras otra pena estan acabando conmigo
-Y yo por ser un hombre tan fuerte he podido resistir
-Ay y no me quiero morir porque me duelen mis hijos
-
-Mejor me voy, mejor me voy
-Como hace el cóndor herido
-Ay! mejor me voy, mejor me voy
-Como hace el cóndor herido.
+Ay y no me quiero morir porque me duelen mis hijos.
         </p>
       </div>
       <div className={style.images_container}>
-        <div className={style.image_wrapper}>
-          <img
-            src="https://picsum.photos/500/500?random=<%= new Date().getTime() %>"
-            alt="Diseño y rediseño de identidad visual"
-          />
-        </div>
-        <div className={style.image_wrapper}>
-          <img
-            src="https://picsum.photos/500/500?random=<%= new Date().getTime() %>"
-            alt="Diseño y rediseño de identidad visual"
-          />
-        </div>
-        <div className={style.image_wrapper}>
-          <img
-            src="https://picsum.photos/500/500?random=<%= new Date().getTime() %>"
-            alt="Diseño y rediseño de identidad visual"
-          />
-        </div>
-        <div className={style.image_wrapper}>
-          <img
-            src="https://picsum.photos/500/500?random=<%= new Date().getTime() %>"
-            alt="Diseño y rediseño de identidad visual"
-          />
-        </div>
-        <div className={style.image_wrapper}>
-          <img
-            src="https://picsum.photos/500/500?random=<%= new Date().getTime() %>"
-            alt="Diseño y rediseño de identidad visual"
-          />
-        </div>
-        <div className={style.image_wrapper}>
-          <img
-            src="https://picsum.photos/500/500?random=<%= new Date().getTime() %>"
-            alt="Diseño y rediseño de identidad visual"
-          />
-        </div>
-        <div className={style.image_wrapper}>
-          <img
-            src="https://picsum.photos/500/500?random=<%= new Date().getTime() %>"
-            alt="Diseño y rediseño de identidad visual"
-          />
-        </div>
-        
+        {bradingImages.map((image) => {
+          return (
+            <div key={image.id} className={style.image_wrapper}>
+              <img
+                src={image.url}
+                alt={image.alt}
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/data/projectsImages.data.js
@@ -0,0 +1,54 @@
+import bvUrl001 from "@assets/images/branding/bv-001.webp";
+import bvUrl002 from "@assets/images/branding/bv-002.webp";
+import bvUrl003 from "@assets/images/branding/bv-003.webp";
+import bvUrl004 from "@assets/images/branding/bv-004.webp";
+import bvUrl005 from "@assets/images/branding/bv-005.webp";
+import bvUrl006 from "@assets/images/branding/bv-006.webp";
+import bvUrl007 from "@assets/images/branding/bv-007.webp";
+
+const brandingImages = [
+  {
+    id: 1,
+    name: "bv-001",
+    url: bvUrl001,
+    alt: "Bunny Velvet - Imagen 1"
+  },
+  {
+    id: 2,
+    name: "bv-002",
+    url: bvUrl002,
+    alt: "Bunny Velvet - Imagen 2"
+  },
+  {
+    id: 3,
+    name: "bv-003",
+    url: bvUrl003,
+    alt: "Bunny Velvet - Imagen 3"
+  },
+  {
+    id: 4,
+    name: "bv-004",
+    url: bvUrl004,
+    alt: "Bunny Velvet - Imagen 4"
+  },
+  {
+    id: 5,
+    name: "bv-005",
+    url: bvUrl005,
+    alt: "Bunny Velvet - Imagen 5"
+  },
+  {
+    id: 6,
+    name: "bv-006",
+    url: bvUrl006,
+    alt: "Bunny Velvet - Imagen 6"
+  },
+  {
+    id: 7,
+    name: "bv-007",
+    url: bvUrl007,
+    alt: "Bunny Velvet - Imagen 7"
+  }
+]
+
+export default brandingImages;
